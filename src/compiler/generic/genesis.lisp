22bea3e3f8ab00a74b38152199bdf0abfbf54e96
Eric Timmons <etimmons@mit.edu>
2021-03-16 09:40:03 -0400

    Add the :SB-PRELINK-LINKAGE-TABLE feature
    This is an internal feature. When present, genesis writes a C helper
    file (linkage-table-prelink-info.c) that populates the C var
    lisp_linkage_values. This variable (originally introduced as part of the
    shrinkwrapping support) contains the true addresses of each variable and
    function in the linkage table.
    
    Then, when the runtime boots instead of using os_dlsym_default and
    required-foreign-symbols to fill the linkage table, the data in
    lisp_linkage_values is used instead. This has the affect of using the system
    linker to prelink the entire linkage table instead of doing it ourselves.
    
    Last, when the core boots, it has to fixup the prelinked entries. This is
    required to make sure the linkage table points to the appropriate guards for
    undefined foreign variables or functions. This cannot be done in the runtime
    because some on some archs the undefined alien function guard is an assembly
    routine generated in lisp. Instead, a NULL entry in lisp_linkage_values
    indicates an undefined entry.
    
    A useful feature for some use cases is to add extra symbols to the linkage
    table at build time that we know we'll need at run time. This is accomplished
    by passing the extra entries to the make process using the
    --extra-linkage-table-entries argument.
    
    The file must contain a single list. Each element is a linkage table entry
    consisting of three elements. The first is a string naming the C symbol. The
    second is T if the symbol is a variable, NIL if it is a function. The third is
    T if the entry is undefined (this is currently pointless, but will become
    useful in a future commit).
    
    Enabling this feature on Windows causes a test failure. The root cause is that
    mingw-w64 implements its trig functions as thin wrappers around fsin and
    friends. This can be wildly inaccurate. When this feature is enabled, the trig
    functions come from wingw-w64's libm. When this feature is disabled, the more
    accurate trig functions from the Windows C runtime are used. See
    https://sourceforge.net/p/mingw-w64/mailman/mingw-w64-public/thread/6141957e.8c6be.157053d0e19.Coremail.lh_mouse%40126.com/#msg35353216
    and https://mingwpy.github.io/issues.html#math-precision-issues for more
    details.
    
    With fix for changes between 2.6.1 and 2.6.2

--- src/compiler/generic/genesis.lisp
+++ src/compiler/generic/genesis.lisp
@@ -2433,6 +2433,47 @@ Legal values for OFFSET are -4, -8, -12, ..."
       ;; C runtime reads the core header entry but cold-init reads the lisp symbol
       (cold-set (cold-intern 'sb-sys:*linkage-info*) (cold-cons *nil-descriptor* v))
       v)))
+
+#+sb-prelink-linkage-table
+(defun foreign-symbols-to-c (output-pathname)
+  (with-open-file (output output-pathname
+                          :direction :output
+                          :if-exists :supersede)
+    (let ((sorted-pairs (sort (%hash-table-alist *cold-foreign-symbol-table*) #'< :key #'cdr)))
+      ;; Needed for uintptr_t. We use the raw uintptr_t as we don't want to have
+      ;; to include any SBCL headers just to get at lispobj.
+      (format output "#include <stdint.h>~%~%")
+
+      ;; Write out the extern definitions. Everything is a void function (even
+      ;; variables) because compilers don't like void variables. Remove
+      ;; alien_linkage_values as we need to write to it, so we should use the
+      ;; actual type.
+      (format output "extern void ~{~A()~^, ~};~%~%"
+              (remove "alien_linkage_values"
+                      (mapcar (lambda (x)
+                                (if (listp (car x))
+                                    (caar x)
+                                    (car x)))
+                              sorted-pairs)
+                      :test #'equal))
+
+      #-win32
+      (format output "uintptr_t __attribute__((weak)) alien_linkage_values[] = {~%")
+      #+win32
+      (format output "uintptr_t alien_linkage_values[] = {~%")
+
+      ;; One address per linkage-table entry, in linkage-index order, matching the
+      ;; format that os_link_runtime consumes (and that elftool emits for an
+      ;; ELF-embedded core): no leading count and no data/-1 markers.  The count
+      ;; is taken from SB-SYS:*LINKAGE-INFO* and data-vs-code from that same table,
+      ;; so emitting them here only shifted every entry by one -- installing the
+      ;; symbol count as entry 0's address (a fault at a tiny address == the count).
+      (dolist (pair sorted-pairs)
+        (format output "  (uintptr_t)&~A,~%" (if (listp (car pair))
+                                                 (caar pair)
+                                                 (car pair))))
+      (format output "};~%"))))
+
 
 ;;;; general machinery for cold-loading FASL files
 
@@ -4261,11 +4302,16 @@ INDEX   LINK-ADDR       FNAME    FUNCTION  NAME
 ;;;   CORE-FILE-NAME gets a Lisp core.
 ;;;   C-HEADER-DIR-NAME gets the path in which to place generated headers
 ;;;   MAP-FILE-NAME gets the name of the textual 'cold-sbcl.map' file
+;;;   LINKAGE-TABLE-PREFILL-INFO-C-NAME gets a .c file used to store the
+;;;     data used to link the runtime before entering Lisp.
 (defun sb-cold:genesis (&key object-file-names foptrace-file-names tls-init
                              defstruct-descriptions
                              build-id
                              core-file-name c-header-dir-name map-file-name
-                             (verbose t))
+                             (verbose t)
+                             linkage-table-prefill-info-c-name
+                             extra-linkage-table-entries)
+  (declare (ignorable linkage-table-prefill-info-c-name))
 
   (when verbose
     (format t
@@ -4284,6 +4330,9 @@ INDEX   LINK-ADDR       FNAME    FUNCTION  NAME
     ;; Prefill some linkage table entries perhaps
     (loop for (name datap) in sb-vm::*alien-linkage-table-predefined-entries*
           do (alien-linkage-table-note-symbol name datap))
+    (loop for (name datap undefinedp) in extra-linkage-table-entries
+          unless undefinedp
+            do (alien-linkage-table-note-symbol name datap))
 
     ;; Now that we've successfully read our only input file (by
     ;; loading the symbol table, if any), it's a good time to ensure
@@ -4415,6 +4464,9 @@ INDEX   LINK-ADDR       FNAME    FUNCTION  NAME
         (sort-initial-methods)
         (resolve-deferred-known-funs)
         (setq foreign-symbols (foreign-symbols-to-core))
+        #+sb-prelink-linkage-table
+        (when linkage-table-prefill-info-c-name
+          (foreign-symbols-to-c linkage-table-prefill-info-c-name))
         (finish-symbols)
         (finalize-load-time-value-noise))
 
